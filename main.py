a1cc692bcd311dfc555451611f49e6834be680b1
ahmadjon2008 <robotoprotecno2008@gmail.com>
2024-02-13 21:01:01 +0500

    Edited commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import openpyxl
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
 from config import *
@@ -7,12 +8,13 @@ from config import *
 bot = Bot(token=Token)
 dp = Dispatcher()
 logging.basicConfig(level=logging.INFO)
+count = 0
 
 
 @dp.message(Command("start"))
 async def start(message: types.Message):
     await message.answer(
-        "<b>Assalomu alaykum!</b>😆", parse_mode="HTML")
+        "<b>Assalomu alaykum!</b>😎", parse_mode="HTML")
     print(message.from_user.full_name, message.from_user.id, message.from_user.username)
 
 
@@ -28,6 +30,22 @@ async def question(message: types.Message):
         close_date=10,
         is_anonymous=False
     )
+    i = count + 1
+
+    work = openpyxl.Workbook()
+    sheet = work.active
+
+    sheet["A1"] = "Ism"
+    sheet["B1"] = "Familiya"
+    sheet["C1"] = "Username"
+    sheet["D1"] = "ID"
+
+    sheet[f"A{i}"] = message.from_user.first_name
+    sheet[f"B{i}"] = message.from_user.last_name
+    sheet[f"C{i}"] = message.from_user.username
+    sheet[f"D{i}"] = message.from_user.id
+
+    work.save('data.xlsx')
 
 
 async def main():
